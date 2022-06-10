46871739f0a3d569f6267ac41a0855c0dbf89bfd
CougarChan <gohan0136@gmail.com>
2022-06-10 12:44:15 -0400

    pushing all code

---
+++ assets/js/style.js
@@ -0,0 +1,10 @@
+var apikey= "d39715860743eb42dfc2b0b68182a2b3";
+
+
+
+function gettingWeather()
+
+cityFormEl.addEventListener("submit", formSubmitHandler);
+searchHistoryBtn.addEventListener("click", previousSearch);
+
+
